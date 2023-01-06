9d2987a17016b38f86935cbaa56e6671071acfe9
william <460706836@qq.com>
2023-01-06 22:02:41 +0800

    add yuv channel check box

--- my_application.h
+++ my_application.h
@@ -16,68 +16,71 @@ class Application {
 public:
   Application() = default;
 
-  void RenderGUI(SDL_Texture* yuv_image_texture)
-  {
+  void RenderGUI(SDL_Texture *yuv_image_texture) {
     showDockSpace();
-    showYUVOptions();
+    showYUVSettings();
 
     showYUVImage(yuv_image_texture);
   }
 
-  void RenderGUI(YUVFileLoader& loader, SDL_Renderer *renderer)
-  {
+  void RenderGUI(YUVFileLoader &loader, SDL_Renderer *renderer) {
     showDockSpace();
-    showYUVOptions();
+    showYUVSettings();
     ImGui::ShowDemoWindow();
-    if(loader.getLoadFilePath().empty()){
+    if (loader.getLoadFilePath().empty()) {
       showDragToOpenWindow();
-    }else
-    {
+    } else {
       showYUVImage(loader, renderer);
     }
   }
 
-
   int format_item_index = 0;
   int yuv_width = 100;
   int yuv_height = 100;
+  bool y_check = true;
+  bool u_check = true;
+  bool v_check = true;
 
 private:
-  void showDockSpace()
-  {
+  void showDockSpace() {
     static bool opt_fullscreen = true;
     static bool opt_padding = false;
     static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
 
-    // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
-    // because it would be confusing to have two docking targets within each others.
-    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-    if (opt_fullscreen)
-    {
-      const ImGuiViewport* viewport = ImGui::GetMainViewport();
+    // We are using the ImGuiWindowFlags_NoDocking flag to make the parent
+    // window not dockable into, because it would be confusing to have two
+    // docking targets within each others.
+    ImGuiWindowFlags window_flags =
+        ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+    if (opt_fullscreen) {
+      const ImGuiViewport *viewport = ImGui::GetMainViewport();
       ImGui::SetNextWindowPos(viewport->WorkPos);
       ImGui::SetNextWindowSize(viewport->WorkSize);
       ImGui::SetNextWindowViewport(viewport->ID);
       ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
       ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-      window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-      window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-    }
-    else
-    {
+      window_flags |= ImGuiWindowFlags_NoTitleBar |
+                      ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize |
+                      ImGuiWindowFlags_NoMove;
+      window_flags |=
+          ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+    } else {
       dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
     }
 
-    // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
-    // and handle the pass-thru hole, so we ask Begin() to not render a background.
+    // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will
+    // render our background and handle the pass-thru hole, so we ask Begin() to
+    // not render a background.
     if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
       window_flags |= ImGuiWindowFlags_NoBackground;
 
-    // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
-    // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
-    // all active windows docked into it will lose their parent and become undocked.
-    // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
-    // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+    // Important: note that we proceed even if Begin() returns false (aka window
+    // is collapsed). This is because we want to keep our DockSpace() active. If
+    // a DockSpace() is inactive, all active windows docked into it will lose
+    // their parent and become undocked. We cannot preserve the docking
+    // relationship between an active window and an inactive docking, otherwise
+    // any change of dockspace/settings would lead to windows being stuck in
+    // limbo and never being visible.
     if (!opt_padding)
       ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
     ImGui::Begin("DockSpace Demo", nullptr, window_flags);
@@ -88,29 +91,47 @@ private:
       ImGui::PopStyleVar(2);
 
     // Submit the DockSpace
-    ImGuiIO& io = ImGui::GetIO();
-    if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
-    {
+    ImGuiIO &io = ImGui::GetIO();
+    if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
       ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
       ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
     }
 
-
-    if (ImGui::BeginMenuBar())
-    {
-      if (ImGui::BeginMenu("Options"))
-      {
-        // Disabling fullscreen would allow the window to be moved to the front of other windows,
-        // which we can't undo at the moment without finer window depth/z control.
+    if (ImGui::BeginMenuBar()) {
+      if (ImGui::BeginMenu("Options")) {
+        // Disabling fullscreen would allow the window to be moved to the front
+        // of other windows, which we can't undo at the moment without finer
+        // window depth/z control.
         ImGui::MenuItem("Fullscreen", NULL, &opt_fullscreen);
         ImGui::MenuItem("Padding", NULL, &opt_padding);
         ImGui::Separator();
 
-        if (ImGui::MenuItem("Flag: NoSplit",                "", (dockspace_flags & ImGuiDockNodeFlags_NoSplit) != 0))                 { dockspace_flags ^= ImGuiDockNodeFlags_NoSplit; }
-        if (ImGui::MenuItem("Flag: NoResize",               "", (dockspace_flags & ImGuiDockNodeFlags_NoResize) != 0))                { dockspace_flags ^= ImGuiDockNodeFlags_NoResize; }
-        if (ImGui::MenuItem("Flag: NoDockingInCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_NoDockingInCentralNode) != 0))  { dockspace_flags ^= ImGuiDockNodeFlags_NoDockingInCentralNode; }
-        if (ImGui::MenuItem("Flag: AutoHideTabBar",         "", (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0))          { dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar; }
-        if (ImGui::MenuItem("Flag: PassthruCentralNode",    "", (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0, opt_fullscreen)) { dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode; }
+        if (ImGui::MenuItem("Flag: NoSplit", "",
+                            (dockspace_flags & ImGuiDockNodeFlags_NoSplit) !=
+                                0)) {
+          dockspace_flags ^= ImGuiDockNodeFlags_NoSplit;
+        }
+        if (ImGui::MenuItem("Flag: NoResize", "",
+                            (dockspace_flags & ImGuiDockNodeFlags_NoResize) !=
+                                0)) {
+          dockspace_flags ^= ImGuiDockNodeFlags_NoResize;
+        }
+        if (ImGui::MenuItem("Flag: NoDockingInCentralNode", "",
+                            (dockspace_flags &
+                             ImGuiDockNodeFlags_NoDockingInCentralNode) != 0)) {
+          dockspace_flags ^= ImGuiDockNodeFlags_NoDockingInCentralNode;
+        }
+        if (ImGui::MenuItem(
+                "Flag: AutoHideTabBar", "",
+                (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0)) {
+          dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar;
+        }
+        if (ImGui::MenuItem(
+                "Flag: PassthruCentralNode", "",
+                (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0,
+                opt_fullscreen)) {
+          dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode;
+        }
         ImGui::Separator();
 
         ImGui::EndMenu();
@@ -121,45 +142,55 @@ private:
     ImGui::End();
   }
 
-  void showYUVOptions()
-  {
-    ImGui::Begin("Options");
+  void showYUVSettings() {
+    ImGui::Begin("Settings");
 
-    const char* items[] = { "YUV420", "YUYV422" , "UYVY422", "YVYU422", "NV12", "NV21"};
+    const char *items[] = {"YUV420",  "YUYV422", "UYVY422",
+                           "YVYU422", "NV12",    "NV21"};
     ImGui::Combo("format", &format_item_index, items, IM_ARRAYSIZE(items));
-
     ImGui::InputInt("width", &yuv_width);
     ImGui::InputInt("height", &yuv_height);
+    ImGui::Separator();
+
+    ImGui::Checkbox("Y", &y_check);
+    ImGui::SameLine();
+    ImGui::Checkbox("U", &u_check);
+    ImGui::SameLine();
+    ImGui::Checkbox("V", &v_check);
+    ImGui::SameLine();
 
     ImGui::End();
   }
 
-  void showYUVImage(SDL_Texture* yuv_image_texture)
-  {
+  void showYUVImage(SDL_Texture *yuv_image_texture) {
     ImGui::Begin("YUV Image");
     ImGui::Text("size = %d x %d", yuv_width, yuv_height);
-    ImGui::Image((void*)(intptr_t)yuv_image_texture, ImVec2(yuv_width, yuv_height));
+    ImGui::Image((void *)(intptr_t)yuv_image_texture,
+                 ImVec2(yuv_width, yuv_height));
     ImGui::End();
   }
 
-  void showYUVImage(YUVFileLoader& loader, SDL_Renderer *renderer)
-  {
-    SDL_Texture* image_texture = loader.updateTexture(YUVFormat(format_item_index), yuv_width, yuv_height,
-                                                          renderer);
+  void showYUVImage(YUVFileLoader &loader, SDL_Renderer *renderer) {
+    YUVSetting setting;
+    setting.format = YUVFormat(format_item_index);
+    setting.width = yuv_width;
+    setting.height = yuv_height;
+
+    SDL_Texture *image_texture = loader.updateTexture(setting, renderer);
     ImGui::Begin("YUV Image");
     ImGui::Text("size = %d x %d", yuv_width, yuv_height);
-    ImGui::Image((void*)(intptr_t)image_texture, ImVec2(yuv_width, yuv_height));
+    ImGui::Image((void *)(intptr_t)image_texture,
+                 ImVec2(yuv_width, yuv_height));
     ImGui::End();
   }
 
-  void showDragToOpenWindow()
-  {
+  void showDragToOpenWindow() {
     auto textCentered = [](std::string text) {
       auto windowWidth = ImGui::GetWindowSize().x;
       auto windowHeight = ImGui::GetWindowSize().y;
 
       auto textSize = ImGui::CalcTextSize(text.c_str());
-      auto textWidth   = textSize.x;
+      auto textWidth = textSize.x;
       auto textHeight = textSize.y;
 
       ImGui::SetCursorPosX((windowWidth - textWidth) * 0.5f);
@@ -172,9 +203,8 @@ private:
     textCentered("Drag YUV File Here To Open");
     ImGui::End();
   }
-
 };
 
-}
+} // namespace MyApp
 
 #endif // SIMPLE_YUV_VIEWER_MY_APPLICATION_H
--- my_yuv_loader.h
+++ my_yuv_loader.h
@@ -16,8 +16,27 @@ extern "C" {
 
 #include <fstream>
 #include <string>
+#include <vector>
 
-enum class YUVFormat { kYUV420 = 0, kYUYV422, kUYVY422, kYVYU422, kNV12, kNV21 };
+enum class YUVFormat {
+  kYUV420 = 0,
+  kYUYV422,
+  kUYVY422,
+  kYVYU422,
+  kNV12,
+  kNV21
+};
+
+struct YUVSetting
+{
+  YUVFormat format;
+  int width;
+  int height;
+  bool show_y;
+  bool show_u;
+  bool show_v;
+};
+
 
 class YUVFileLoader {
 public:
@@ -31,6 +50,9 @@ public:
       in.seekg(0, std::ios::beg);
       in.read(&file_contents_[0], file_contents_.size());
       in.close();
+
+      fake_128_data_.resize(file_contents_.size(), 128);
+
       return true;
     } else {
       filepath_ = "";
@@ -41,23 +63,23 @@ public:
 
   const std::string &getLoadFilePath() const { return filepath_; }
 
-  SDL_Texture *updateTexture(YUVFormat format, size_t width, size_t height,
-                             SDL_Renderer *renderer) {
-
-    createTextureIfNeed(format, width, height, renderer);
-
-    switch (format) {
+  SDL_Texture* updateTexture(const YUVSetting& setting,
+                             SDL_Renderer *renderer
+                             )
+  {
+    createTextureIfNeed(setting, renderer);
+    switch (setting.format) {
     case YUVFormat::kYUV420:
-      updateTextureYUV420(width, height);
+      updateTextureYUV420(setting);
       break;
     case YUVFormat::kYUYV422:
     case YUVFormat::kYVYU422:
     case YUVFormat::kUYVY422:
-      updateTexturePacketYUV422(width);
+      updateTexturePacketYUV422(setting);
       break;
     case YUVFormat::kNV12:
     case YUVFormat::kNV21:
-      updateTextureNV(width, height);
+      updateTextureNV(setting);
       break;
     }
 
@@ -99,23 +121,23 @@ private:
                                  SDL_TEXTUREACCESS_STREAMING, width, height);
   }
 
-  void createTextureIfNeed(YUVFormat format, size_t width, size_t height,
+  void createTextureIfNeed(const YUVSetting& setting,
                            SDL_Renderer *renderer) {
     if (texture_ == nullptr) {
-      createTexture(format, width, height, renderer);
+      createTexture(setting.format, setting.width, setting.height, renderer);
     } else {
       auto tex_width = 0;
       auto tex_height = 0;
       Uint32 tex_format = 0;
       SDL_QueryTexture(texture_, &tex_format, NULL, &tex_width, &tex_height);
-      if (width != tex_width || height != tex_height ||
-          tex_format != Uint32(format)) {
-        createTexture(format, width, height, renderer);
+      if (setting.width != tex_width || setting.height != tex_height ||
+          tex_format != Uint32(setting.format)) {
+        createTexture(setting.format, setting.width, setting.height, renderer);
       }
     }
   }
 
-  void updateTextureNV(size_t width, size_t height) {
+  void updateTextureNV(const YUVSetting& setting) {
     if (file_contents_.empty()) {
       return;
     }
@@ -123,19 +145,28 @@ private:
     auto content_size = file_contents_.size();
     auto *yuv_data = reinterpret_cast<const uint8_t *>(file_contents_.data());
     auto *y_plane = yuv_data;
-    size_t y_stride = width;
+    size_t y_stride = setting.width;
 
-    auto uv_offset = width * height;
+    auto uv_offset = setting.width * setting.height;
     uv_offset = (uv_offset > content_size) ? (content_size) : (uv_offset);
     auto *uv_plane = yuv_data + uv_offset;
-    size_t uv_stride = width;
+    size_t uv_stride = setting.width;
 
     SDL_UpdateNVTexture(texture_, nullptr, y_plane, y_stride, uv_plane,
                         uv_stride);
   }
 
-  void updateTexturePacketYUV422(size_t width)
-  {
+  void updateTexturePacketYUV422(const YUVSetting& setting) {
+    if (file_contents_.empty()) {
+      return;
+    }
+
+    auto *yuv_data = reinterpret_cast<const uint8_t *>(file_contents_.data());
+    auto pitch = 2 * setting.width;
+    SDL_UpdateTexture(texture_, nullptr, yuv_data, pitch);
+  }
+
+  void updateTexturePacketYUV422(size_t width) {
     if (file_contents_.empty()) {
       return;
     }
@@ -145,7 +176,7 @@ private:
     SDL_UpdateTexture(texture_, nullptr, yuv_data, pitch);
   }
 
-  void updateTextureYUV444(size_t width, size_t height) {
+  void updateTextureYUV420(const YUVSetting& setting) {
     if (file_contents_.empty()) {
       return;
     }
@@ -153,53 +184,17 @@ private:
     auto content_size = file_contents_.size();
     auto *yuv_data = reinterpret_cast<const uint8_t *>(file_contents_.data());
     auto *y_plane = yuv_data;
-    size_t y_stride = width;
+    size_t y_stride = setting.width;
 
-    auto u_offset = width * height;
+    auto u_offset = setting.width * setting.height;
     u_offset = (u_offset > content_size) ? (content_size) : (u_offset);
     auto *u_plane = yuv_data + u_offset;
-    size_t u_stride = width;
+    size_t u_stride = setting.width / 2;
 
-    auto v_offset = u_offset + (width * height);
+    auto v_offset = u_offset + (setting.width * setting.height) / 4;
     v_offset = (v_offset > content_size) ? (content_size) : (v_offset);
     auto *v_plane = yuv_data + v_offset;
-    size_t v_stride = width;
-
-    SDL_UpdateYUVTexture(
-        texture_, // the texture to update
-        nullptr,  // a pointer to the rectangle of pixels to update, or NULL to
-                  // update the entire texture
-        y_plane,  // the raw pixel data for the Y plane
-        y_stride, // the number of bytes between rows of pixel data for the Y
-                  // plane
-        u_plane,  // the raw pixel data for the U plane
-        u_stride, // the number of bytes between rows of pixel data for the U
-                  // plane
-        v_plane,  // the raw pixel data for the V plane
-        v_stride  // the number of bytes between rows of pixel data for the V
-                  // plane
-    );
-  }
-
-  void updateTextureYUV420(size_t width, size_t height) {
-    if (file_contents_.empty()) {
-      return;
-    }
-
-    auto content_size = file_contents_.size();
-    auto *yuv_data = reinterpret_cast<const uint8_t *>(file_contents_.data());
-    auto *y_plane = yuv_data;
-    size_t y_stride = width;
-
-    auto u_offset = width * height;
-    u_offset = (u_offset > content_size) ? (content_size) : (u_offset);
-    auto *u_plane = yuv_data + u_offset;
-    size_t u_stride = width / 2;
-
-    auto v_offset = u_offset + (width * height) / 4;
-    v_offset = (v_offset > content_size) ? (content_size) : (v_offset);
-    auto *v_plane = yuv_data + v_offset;
-    size_t v_stride = width / 2;
+    size_t v_stride = setting.width / 2;
 
     SDL_UpdateYUVTexture(
         texture_, // the texture to update
@@ -218,6 +213,7 @@ private:
   }
 
   std::string file_contents_;
+  std::vector<uint8_t> fake_128_data_;
   std::string filepath_;
   SDL_Texture *texture_{nullptr};
 };
